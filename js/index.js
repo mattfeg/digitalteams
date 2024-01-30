19cdc1fca2735b74366b08d1e922e9fd0f99a9d7
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-01-30 12:08:42 -0300

    ouvindo o evento de criar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
+let logo = document.querySelector('.logo');
 criarBtn.onclick = () => {
-    // alert("clicou");
-    // console.log("clicou aqui");
     overlay.classList.add('show');
     formCriar.classList.add('show');
 }
+
+fecharBtn.onclick = () => {
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+}
+
+overlay.onclick = () => {
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+}
+
+formCriar.onsubmit = () => {
+    event.preventDefault();
+
+    alert(nome.value+' '+capacidade.value);
+    listTeams.innerHTML = '';
+    listTeams.innerHTML = `
+        <li>
+            <h4>Nome do Team <box-icon name='show'></box-icon></h4>
+            <h1>0 <span>/ 0</span></h1>
+            <div class="actions">
+                <button>adicionar</button>
+                <button><box-icon name='trash'></box-icon></button>
+            </div>
+        </li>
+    `;
+    overlay.classList.remove('show');
+    formCriar.classList.remove('show');
+}
+
