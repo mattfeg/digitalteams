4837eb8a03602d00cf8ec722b1ade00f967f29fd
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-02-15 12:07:48 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-let logo = document.querySelector('.logo');
+let teams = [];
+
 criarBtn.onclick = () => {
     overlay.classList.add('show');
     formCriar.classList.add('show');
@@ -16,19 +17,22 @@ overlay.onclick = () => {
 
 formCriar.onsubmit = () => {
     event.preventDefault();
-
-    alert(nome.value+' '+capacidade.value);
-    listTeams.innerHTML = '';
-    listTeams.innerHTML = `
-        <li>
-            <h4>Nome do Team <box-icon name='show'></box-icon></h4>
-            <h1>0 <span>/ 0</span></h1>
-            <div class="actions">
-                <button>adicionar</button>
-                <button><box-icon name='trash'></box-icon></button>
-            </div>
-        </li>
-    `;
+    teams.push({
+        name: nome.value,
+        capacity: capacidade.value,
+        members: []
+    });
+    // listTeams.innerHTML = '';
+    // listTeams.innerHTML = `
+    //     <li>
+    //         <h4>${nome.value} <box-icon name='show'></box-icon></h4>
+    //         <h1>0 <span>/ ${capacidade.value}</span></h1>
+    //         <div class="actions">
+    //             <button>adicionar</button>
+    //             <button><box-icon name='trash'></box-icon></button>
+    //         </div>
+    //     </li>
+    // `;
     overlay.classList.remove('show');
     formCriar.classList.remove('show');
 }
