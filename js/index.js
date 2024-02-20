13a19135e2837cfafc51704c6712ac1282bc2f23
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-02-20 11:59:44 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -22,18 +22,25 @@ formCriar.onsubmit = () => {
         capacity: capacidade.value,
         members: []
     });
-    // listTeams.innerHTML = '';
-    // listTeams.innerHTML = `
-    //     <li>
-    //         <h4>${nome.value} <box-icon name='show'></box-icon></h4>
-    //         <h1>0 <span>/ ${capacidade.value}</span></h1>
-    //         <div class="actions">
-    //             <button>adicionar</button>
-    //             <button><box-icon name='trash'></box-icon></button>
-    //         </div>
-    //     </li>
-    // `;
+    adicionarCards();
     overlay.classList.remove('show');
     formCriar.classList.remove('show');
 }
+
+function adicionarCards(){
+    listTeams.innerHTML = '';
+    for(let i = 0; i < teams.length; i++){
+        listTeams.innerHTML += `
+            <li>
+                <h4>${teams[i].name} <box-icon name='show'></box-icon></h4>
+                <h1>0 <span>/ ${teams[i].capacity}</span></h1>
+                <div class="actions">
+                    <button>adicionar</button>
+                    <button><box-icon name='trash'></box-icon></button>
+                </div>
+            </li>
+        `;
+    }
+}
+
 
