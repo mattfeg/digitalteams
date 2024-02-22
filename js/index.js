e6e0897eb112a79eee6bfaf0503e6bbdb137ecf2
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-02-22 12:23:51 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -10,37 +10,83 @@ fecharBtn.onclick = () => {
     formCriar.classList.remove('show');
 }
 
+fecharParticipanteBtn.onclick = () => {
+    overlay.classList.remove('show');
+    formParticipante.classList.remove('show');
+}
+
 overlay.onclick = () => {
     overlay.classList.remove('show');
     formCriar.classList.remove('show');
+    formParticipante.classList.remove('show');
 }
 
 formCriar.onsubmit = () => {
     event.preventDefault();
-    teams.push({
-        name: nome.value,
-        capacity: capacidade.value,
-        members: []
-    });
-    adicionarCards();
-    overlay.classList.remove('show');
-    formCriar.classList.remove('show');
+    if(verificarLista(nome.value)){
+        alert("Este nome já está em uso");
+    }else{
+        teams.push({
+            name: nome.value,
+            capacity: capacidade.value,
+            members: []
+        });
+        adicionarCards();
+        overlay.classList.remove('show');
+        formCriar.classList.remove('show');
+    }
+}
+
+formParticipante.onsubmit = () => {
+    event.preventDefault();
+    teams[Number(teamID.value)].members.push(nomeParticipante.value);
+    alert("Participante inserido com sucesso!");
+    formParticipante.reset();
 }
 
 function adicionarCards(){
     listTeams.innerHTML = '';
+    if(teams.length === 0){
+        listTeams.innerHTML = '<li class="noTeams"><h4>Nenhum team criado ainda</h4></li>';
+        return;
+    }
     for(let i = 0; i < teams.length; i++){
         listTeams.innerHTML += `
             <li>
                 <h4>${teams[i].name} <box-icon name='show'></box-icon></h4>
                 <h1>0 <span>/ ${teams[i].capacity}</span></h1>
                 <div class="actions">
-                    <button>adicionar</button>
-                    <button><box-icon name='trash'></box-icon></button>
+                    <button onClick="mostrarFormParticipante(${i})">adicionar</button>
+                    <button onClick="removerCard(${i})"><box-icon name='trash'></box-icon></button>
                 </div>
             </li>
         `;
     }
 }
 
+function removerCard(indice){
+    let listaAuxiliar = [];
+    for(let i = 0; i < teams.length; i++){
+        if(i != indice){
+            listaAuxiliar.push(teams[i]);
+        }
+    }
+    teams = listaAuxiliar;
+    adicionarCards();
+}
 
+function verificarLista(nomeDoTeam){
+    let achou = false;
+    for(let i = 0; i < teams.length; i++){
+        if(teams[i].name === nomeDoTeam){
+            achou = true;
+        }
+    }
+    return achou;    
+}
+
+function mostrarFormParticipante(indice){
+    overlay.classList.add("show");
+    formParticipante.classList.add("show");
+    teamID.value = indice;
+}
